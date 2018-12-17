7ae0fa2ae856d486cd24d620e44866a45a314928
tangkehu <tangkehu@cecgw.cn>
2018-12-17 15:04:15 +0800

    商品管理主页设计完毕

--- app/goods/views.py
+++ app/goods/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 from . import goods_bp
 
@@ -7,3 +7,12 @@ from . import goods_bp
 def index():
     return render_template('goods/index.html')
 
+
+@goods_bp.route('/update_goods', methods=['GET', 'POST'])
+def update_goods():
+    return render_template('goods/update.html')
+
+
+@goods_bp.route('/delete_goods')
+def delete_goods():
+    return redirect(url_for('.index'))
