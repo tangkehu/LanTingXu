531627add7b72a03e4369b565929d1bc1b819dee
tangkehu <tangkehu@cecgw.cn>
2018-12-25 14:46:38 +0800

    添加flash提示消息，修改商品删除机制为POST，修复商品添加无商品图的BUG

--- app/goods/forms.py
+++ app/goods/forms.py
@@ -6,8 +6,11 @@ from wtforms.validators import DataRequired, Length
 class GoodsForm(FlaskForm):
     name = StringField('商品名', validators=[DataRequired('请输入商品名'), Length(1, 64, '商品名不超过64个字符')])
     rent = IntegerField('租金', validators=[DataRequired('请输入租金')])
-    submit = SubmitField('添加')
 
     def set_data(self, name, rent):
         self.name.data = name
         self.rent.data = rent
+
+
+class GoodsDeleteForm(FlaskForm):
+    goods_id = IntegerField(validators=[DataRequired('未找到要删除的对象。')])
