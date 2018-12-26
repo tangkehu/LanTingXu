3548659873a67fa705446d3264e498155921592d
tangkehu <tangkehu@cecgw.cn>
2018-12-26 15:00:25 +0800

    更改商品添加表单中押金的验证方式为可选

--- app/goods/forms.py
+++ app/goods/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Optional
 
 
 class GoodsForm(FlaskForm):
     name = StringField('商品名', validators=[DataRequired('请输入商品名'), Length(1, 64, '商品名不超过64个字符')])
     rent = IntegerField('租金', validators=[DataRequired('请输入租金')])
-    cash_pledge = IntegerField('押金', validators=[DataRequired('请输入押金')])
+    cash_pledge = IntegerField('押金', validators=[Optional()])
     brand = StringField('品牌', validators=[Length(-1, 64, '品牌名不超过64个字符')])
     details = TextAreaField('详情', validators=[Length(-1, 500, '详情描述不超过500个字符')])
 
