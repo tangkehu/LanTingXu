c0bedd6370a87146ab2da74dcbb8d8e50c164abe
tangkehu <tangkehu@cecgw.cn>
2019-01-14 15:51:31 +0800

    添加无价格的商品呈现列

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, jsonify
 
 from . import main_bp
 from app.models import Goods
@@ -14,3 +14,17 @@ def index():
 def goods_show(goods_id):
     goods = Goods.query.get_or_404(goods_id)
     return render_template('main/goods_show.html', goods=goods)
+
+
+@main_bp.route('/goods_no_price')
+@main_bp.route('/goods_no_price/<int:goods_id>')
+def goods_no_price(goods_id=None):
+    if goods_id:
+        goods = Goods.query.get_or_404(goods_id)
+        return jsonify({'name': goods.name,
+                        'cash_pledge': goods.cash_pledge,
+                        'size': goods.size,
+                        'quantity': goods.quantity,
+                        'images': [item.filename_m for item in goods.img.all()]})
+    goods_list = Goods.query.order_by(Goods.create_time.desc()).all()
+    return render_template('main/goods_no_price.html', goods_list=goods_list)
