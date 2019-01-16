3e165b95df9df8053e8fbd20ab2944d56ba11a28
tangkehu <tangkehu@cecgw.cn>
2019-01-16 14:45:32 +0800

    添加错误处理，将错误日志发送至管理员邮箱

--- config.py
+++ config.py
@@ -6,6 +6,13 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # 不追踪对象的修改，减少内存使用
     GOODS_IMG_PATH = os.path.join(os.getcwd(), 'app'+os.sep+'static'+os.sep+'img_goods')
 
+    MAIL_SERVER = 'smtp.163.com'
+    MAIL_PORT = 25
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_SENDER = 'www.lanting.live <{}>'.format(MAIL_USERNAME)
+    MAIL_ADMINS = ['329937872@qq.com']
+
 
 class DevelopmentConfig(Config):
     SERVER_NAME = 'lanting.com'  # 用于配置子域名以及url_for生成完整的URL
