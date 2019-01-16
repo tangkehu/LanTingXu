4e05d7cc2de17abdbfe2ef9183da4188368d38e1
tangkehu <tangkehu@cecgw.cn>
2019-01-16 17:29:12 +0800

    465端口发送SSL加密邮件

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ class Config:
     GOODS_IMG_PATH = os.path.join(os.getcwd(), 'app'+os.sep+'static'+os.sep+'img_goods')
 
     MAIL_SERVER = 'smtp.163.com'
-    MAIL_PORT = 25
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
     MAIL_SENDER = 'www.lanting.live <{}>'.format(MAIL_USERNAME)
