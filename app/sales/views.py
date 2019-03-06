12a2d02c185c022b4560de70470dcb7dc0a52253
tangkehu <tangkehu@cecgw.cn>
2019-03-06 17:34:20 +0800

    新增订单界面

--- app/sales/views.py
+++ app/sales/views.py
@@ -6,3 +6,8 @@ from . import sales_bp
 @sales_bp.route('/day')
 def day():
     return render_template('sales/day.html')
+
+
+@sales_bp.route('/order_update', methods=['GET', 'POST'])
+def order_update():
+    return render_template('sales/order_update.html')
