8ae1aa5471033705c250da612c606a0cb545c3dc
tangkehu <tangkehu@cecgw.cn>
2019-03-11 17:33:51 +0800

    完成商品添加，待完成订单页面

--- app/sales/views.py
+++ app/sales/views.py
@@ -1,5 +1,5 @@
 
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, flash
 from . import sales_bp
 from .forms import SalesOrderForm
 from ..models import Goods, SalesOrder, RelationOrderGoods
@@ -8,30 +8,10 @@ from ..models import Goods, SalesOrder, RelationOrderGoods
 @sales_bp.route('/day')
 def day():
     # TODO: 后续按天查找
+    # 在每次GET请求时清理无效订单
     SalesOrder.clear_invalid()
     order_list = SalesOrder.query.filter(SalesOrder.status != 0).order_by(SalesOrder.create_time.desc()).all()
     return render_template('sales/day.html', order_list=order_list)
-
-
-@sales_bp.route('/order_update/<int:order_id>', methods=['GET', 'POST'])
-def order_update(order_id):
-    current_order = SalesOrder.query.get_or_404(order_id)
-    form = SalesOrderForm()
-
-    if request.method == "GET":
-        current_order.sum_price()
-        form.set_data(current_order)
-
-    if form.validate_on_submit():
-        return redirect(url_for('.day'))
-
-    return render_template('sales/order_update.html', form=form)
-
-
-@sales_bp.route('/order_info/<int:order_id>')
-def order_info(order_id):
-    goods_list = []
-    return render_template('sales/order_info.html', goods_list=goods_list)
 
 
 @sales_bp.route('/order_add_goods')
@@ -43,7 +23,7 @@ def order_add_goods(order_id=None):
         current_order = SalesOrder.query.get_or_404(order_id)
 
     if request.method == 'POST':
-        # 接收ajax发送的post请求
+        # 接收ajax发送的post请求，处理商品的追加和移除
         goods_id = request.form.get('goods_id')
         shift_type = request.form.get('shift_type')
         if shift_type == "append":
@@ -56,7 +36,35 @@ def order_add_goods(order_id=None):
     return render_template('sales/order_add_goods.html', goods_list=goods_list, order_id=order_id)
 
 
-@sales_bp.route('/goods_search')
-def goods_search():
+@sales_bp.route('/goods_search/<int:order_id>', methods=['GET', 'POST'])
+def goods_search(order_id):
     goods_list = []
-    return render_template('sales/search_goods.html', goods_list=goods_list)
+    if request.method == "POST":
+        search_word=request.form.get('search_word')
+        if search_word.isdigit():
+            goods_list = Goods.query.filter_by(id=int(search_word)).all()
+        else:
+            goods_list = Goods.query.filter(Goods.name.like('%{}%'.format(search_word))).all()
+    return render_template('sales/search_goods.html', goods_list=goods_list, order_id=order_id)
+
+
+@sales_bp.route('/order_update/<int:order_id>', methods=['GET', 'POST'])
+def order_update(order_id):
+    current_order = SalesOrder.query.get_or_404(order_id)
+    form = SalesOrderForm()
+
+    if request.method == "GET":
+        form.set_data(current_order)
+
+    if form.validate_on_submit():
+        current_order.salesman_update(form.total_real.data, form.pay_type.data, form.pay_status.data, form.remarks.data)
+        flash('新订单创建成功')
+        return redirect(url_for('.day'))
+
+    return render_template('sales/order_update.html', form=form)
+
+
+@sales_bp.route('/order_info/<int:order_id>')
+def order_info(order_id):
+    goods_list = []
+    return render_template('sales/order_info.html', goods_list=goods_list)
