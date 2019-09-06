932a6bbfcc06f9338a4f7de7ffa1669e3d0e90c1
tangkehu <tangkehu@cecgw.cn>
2019-09-06 15:36:58 +0800

    临时修改主页以备案

--- app/main/views.py
+++ app/main/views.py
@@ -5,19 +5,19 @@ from app import db
 from app.models import Goods, GoodsType, GoodsImg, HomePage
 
 
-@main_bp.route('/')
-@main_bp.route('/<int:type_id>')
-@main_bp.route('/index')
-@main_bp.route('/index/<int:type_id>')
-def index(type_id=None):
-    type_id = type_id if type_id else GoodsType.query.first().id
-    type_list = GoodsType.query.all()
-    body = HomePage.query.first()
-    current_type = GoodsType.query.get_or_404(type_id).name
-    # return redirect(url_for('.index_new'))
-    return render_template('main/blank.html')
-    # return render_template('main/index.html',
-    #                        type_id=type_id, type_list=type_list, current_type=current_type, body=body)
+# @main_bp.route('/')
+# @main_bp.route('/<int:type_id>')
+# @main_bp.route('/index')
+# @main_bp.route('/index/<int:type_id>')
+# def index(type_id=None):
+#     type_id = type_id if type_id else GoodsType.query.first().id
+#     type_list = GoodsType.query.all()
+#     body = HomePage.query.first()
+#     current_type = GoodsType.query.get_or_404(type_id).name
+#     return redirect(url_for('.index_new'))
+#     return render_template('main/blank.html')
+#     return render_template('main/index.html',
+#                            type_id=type_id, type_list=type_list, current_type=current_type, body=body)
 
 
 @main_bp.route('/goods_list/<int:tid>/<int:page>')
@@ -37,6 +37,10 @@ def goods_show(goods_id):
     return render_template('main/goods_show.html', goods=goods)
 
 
+@main_bp.route('/')
+@main_bp.route('/<int:goods_id>')
+@main_bp.route('/index')
+@main_bp.route('/index/<int:goods_id>')
 @main_bp.route('/goods_no_price')
 @main_bp.route('/goods_no_price/<int:goods_id>')
 def goods_no_price(goods_id=None):
