8feb44a7fe601b65adf74fad8a88ebf256c95a39
tangkehu <tangkehu@cecgw.cn>
2019-09-17 14:33:07 +0800

    恢复主页，新增防备案页面

--- app/main/views.py
+++ app/main/views.py
@@ -1,23 +1,29 @@
-from flask import render_template, jsonify, current_app, url_for, redirect
+from flask import render_template, jsonify, current_app, url_for
 
 from . import main_bp
 from app import db
 from app.models import Goods, GoodsType, GoodsImg, HomePage
 
 
-# @main_bp.route('/')
-# @main_bp.route('/<int:type_id>')
-# @main_bp.route('/index')
-# @main_bp.route('/index/<int:type_id>')
-# def index(type_id=None):
-#     type_id = type_id if type_id else GoodsType.query.first().id
-#     type_list = GoodsType.query.all()
-#     body = HomePage.query.first()
-#     current_type = GoodsType.query.get_or_404(type_id).name
-#     return redirect(url_for('.index_new'))
-#     return render_template('main/blank.html')
-#     return render_template('main/index.html',
-#                            type_id=type_id, type_list=type_list, current_type=current_type, body=body)
+@main_bp.route('/')
+@main_bp.route('/<int:type_id>')
+@main_bp.route('/index')
+@main_bp.route('/index/<int:type_id>')
+def index(type_id=None):
+    body = HomePage.query.first()
+    type_id = type_id if type_id else GoodsType.query.filter_by(sequence=1).first().id
+    type_list = GoodsType.query.all()
+    return render_template('main/index.html', body=body, type_id=type_id, type_list=type_list)
+
+
+@main_bp.route('/index_new')
+@main_bp.route('/index_new/<int:type_id>')
+def index_new(type_id=None):
+    """ 备用首页 """
+    body = HomePage.query.first()
+    type_id = type_id if type_id else GoodsType.query.filter_by(sequence=1).first().id
+    type_list = GoodsType.query.all()
+    return render_template('main/index.html', body=body, type_id=type_id, type_list=type_list)
 
 
 @main_bp.route('/goods_list/<int:tid>/<int:page>')
@@ -37,28 +43,18 @@ def goods_show(goods_id):
     return render_template('main/goods_show.html', goods=goods)
 
 
-@main_bp.route('/')
-@main_bp.route('/<int:goods_id>')
-@main_bp.route('/index')
-@main_bp.route('/index/<int:goods_id>')
-@main_bp.route('/goods_no_price')
-@main_bp.route('/goods_no_price/<int:goods_id>')
-def goods_no_price(goods_id=None):
-    if goods_id:
-        goods = Goods.query.get_or_404(goods_id)
-        return jsonify({'name': goods.name,
-                        'cash_pledge': goods.cash_pledge,
-                        'size': goods.size,
-                        'quantity': goods.quantity,
-                        'images': [item.filename_l for item in goods.img.all()]})
-    goods_li = Goods.query.order_by(Goods.create_time.desc()).all()
-    return render_template('main/goods_no_price.html', goods_list=goods_li)
-
-
-@main_bp.route('/index_new')
-@main_bp.route('/index_new/<int:type_id>')
-def index_new(type_id=None):
-    body = HomePage.query.first()
-    type_id = type_id if type_id else GoodsType.query.filter_by(sequence=1).first().id
-    type_list = GoodsType.query.all()
-    return render_template('main/index_new.html', body=body, type_id=type_id, type_list=type_list)
+# @main_bp.route('/')
+# @main_bp.route('/<int:goods_id>')
+# @main_bp.route('/index')
+# @main_bp.route('/index/<int:goods_id>')
+# def resist_regulation(goods_id=None):
+#     """ 该路由在网站备案时开启，并将真实主页关闭 """
+#     if goods_id:
+#         goods = Goods.query.get_or_404(goods_id)
+#         return jsonify({'name': goods.name,
+#                         'cash_pledge': goods.cash_pledge,
+#                         'size': goods.size,
+#                         'quantity': goods.quantity,
+#                         'images': [item.filename_l for item in goods.img.all()]})
+#     goods_li = Goods.query.order_by(Goods.create_time.desc()).all()
+#     return render_template('main/resist_regulation.html', goods_list=goods_li)
