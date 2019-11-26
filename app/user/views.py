516bda62eada683c7c77e4805708276b544a90a9
Tang <329937872@qq.com>
2019-11-26 20:15:02 +0800

    新增个人首页完成

--- app/user/views.py
+++ app/user/views.py
@@ -1,12 +1,21 @@
 
-from flask import render_template
+from flask import render_template, request
 
-from app.models import User
+from app.models import User, Goods
 from . import user_bp
 
 
 @user_bp.route('/<int:uid>')
-@user_bp.route('/<int:uid>/<int:tid>')
-def index(uid, tid=None):
+def index(uid):
+    """
+    接受的http get 参数包括 tid,
+    """
     user_obj = User.query.get_or_404(uid)
-    return render_template('user/base.html', user_obj=user_obj, type_id=tid)
+    args = request.args.to_dict()
+    tid = int(args.get('tid', 0))
+
+    if tid:
+        goods_li = user_obj.goods.filter(Goods.status == True, Goods.type_id == tid).order_by(Goods.price.asc()).all()
+    else:
+        goods_li = user_obj.goods.filter(Goods.status == True).order_by(Goods.updata_time.desc()).all()
+    return render_template('user/base.html', user_obj=user_obj, type_id=tid, goods_li=goods_li)
