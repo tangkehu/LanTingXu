66b732415626b5144cbf330d3b97fa7dbf9c0ae3
tangkehu <tangkehu@cecgw.cn>
2019-11-27 15:06:11 +0800

    新增账号管理功能

--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,9 @@
 
-from flask import render_template, request
+from flask import render_template, request, flash, redirect, url_for
+from flask_login import current_user, login_required
 
 from app.models import User, Goods
+from app.manage.forms import UserForm
 from . import user_bp
 
 
@@ -19,3 +21,22 @@ def index(uid):
     else:
         goods_li = user_obj.goods.filter(Goods.status == True).order_by(Goods.updata_time.desc()).all()
     return render_template('user/base.html', user_obj=user_obj, type_id=tid, goods_li=goods_li)
+
+
+@user_bp.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+    user_obj = current_user._get_current_object()
+    form = UserForm(user_obj)
+
+    if request.method == 'GET':
+        form.set_data()
+
+    if form.validate_on_submit():
+        kwargs = {'username': form.username.data, 'email': form.email.data, 'phone_number': form.phone_number.data,
+                  'resume': form.resume.data, 'password': form.password.data}
+        user_obj.edit(**kwargs)
+        flash('账号信息修改成功！')
+        return redirect(url_for('.account'))
+
+    return render_template('user/account.html', form=form)
